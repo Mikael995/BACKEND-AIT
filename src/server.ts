76195712a7ed51bb4928ec238899e40a44229332
Junior Kissi <amondkissi@gmail.com>
2026-02-20 17:29:46 -0600

    8080

--- src/server.ts
+++ src/server.ts
@@ -40,7 +40,8 @@ app.use(cors({
   origin: [
     "https://www.ivoriansintexas.com", 
     "https://ivoriansintexas.com", 
-    "http://localhost:5173"
+    "http://localhost:5173",
+    "http://localhost:8080"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
